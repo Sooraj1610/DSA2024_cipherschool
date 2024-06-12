66f3860ffb9497324a1a73169cc69995ef837a37
Sooraj016 <Sooraj16101002@gmail.com>
2024-06-12 15:39:19 +0530

    description of constructor

--- class2.cpp
+++ class2.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+//A constructor is a special member function that is called automatically when the object is created .It has no return type.#include<iostream>
 using namespace std;
 class Employee{
     public:
-    Employee()
+    //default constructor
+    Employee()  
     {
         cout<<"Constructor Called ";
     }
